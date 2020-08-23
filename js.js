e48fb640767f25d10161f547825d0981fd8a827d
Rentib <70008415+Rentib@users.noreply.github.com>
2020-08-23 20:44:43 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,9 +1,11 @@
 /*------------------------------------------------------------------------------------------------------*/
+var k = 0;
 const navSlide = () => {
 	const burger = document.querySelector('.burger');
 	const links = document.querySelector('.nav-links');
 	const navLinks = document.querySelectorAll('.nav-links li');
 	burger.addEventListener('click',()=>{
+		k = k ^ 1;
 		links.classList.toggle('nav-links-active');
 		burger.classList.toggle('change');
 		navLinks.forEach((link, index) => {
@@ -15,6 +17,23 @@ const navSlide = () => {
 			}
 		});
 	});
+	navLinks.forEach(item => {
+		item.addEventListener('click', event => {
+			if(k == 1){
+				links.classList.toggle('nav-links-active');
+				burger.classList.toggle('change');
+				navLinks.forEach((link, index) => {
+					if(link.style.animation){
+						link.style.animation = '';
+					}
+					else{
+						link.style.animation = `linksFade 0.5s ease forwards ${index / 7 + 0.4}s`;
+					}
+				});
+			}
+			k = 0;
+		});
+	})
 }
 navSlide();
 /*------------------------------------------------------------------------------------------------------*/
@@ -26,4 +45,13 @@ const topButton = () => {
   	}));
 }
 topButton();
+/*------------------------------------------------------------------------------------------------------*/
+const scrollFunction = () => {
+	const btn = document.querySelector(".topButton");
+  	btn.addEventListener('click', () => window.scrollTo({
+    	top: 0,
+    	behavior: 'smooth',
+  	}));
+}
+scrollFunction();
 /*------------------------------------------------------------------------------------------------------*/
