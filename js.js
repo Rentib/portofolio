234daa8c8ea7b046f7a364104b03b3a780fb178d
Rentib <70008415+Rentib@users.noreply.github.com>
2020-08-23 20:20:34 +0200

    Update js.js

--- js.js
+++ js.js
@@ -26,13 +26,4 @@ const topButton = () => {
   	}));
 }
 topButton();
-
-/*------------------------------------------------------------------------------------------------------*/
-function fix(){//unhover the hover
-    var el = this;
-    var par = el.parentNode;
-    var next = el.nextSibling;
-    par.removeChild(el);
-    setTimeout(function() {par.insertBefore(el, next);}, 0)
-}
 /*------------------------------------------------------------------------------------------------------*/
