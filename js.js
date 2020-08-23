9299251a1ae72047c2bfc5d772fe56d91c2d13d2
Rentib <70008415+Rentib@users.noreply.github.com>
2020-08-23 11:17:46 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,8 +1,8 @@
+/*------------------------------------------------------------------------------------------------------*/
 const navSlide = () => {
 	const burger = document.querySelector('.burger');
 	const links = document.querySelector('.nav-links');
 	const navLinks = document.querySelectorAll('.nav-links li');
-
 	burger.addEventListener('click',()=>{
 		links.classList.toggle('nav-links-active');
 		burger.classList.toggle('change');
@@ -17,3 +17,14 @@ const navSlide = () => {
 	});
 }
 navSlide();
+/*----------------PRZYCISK NA GÓRĘ----------------------------------------------------------------------*/
+const topButton = () => {
+	const btn = document.querySelector(".topButton");
+  	btn.addEventListener('click', () => window.scrollTo({
+    	top: 0,
+    	behavior: 'smooth',
+  	}));
+}
+topButton();
+
+/*------------------------------------------------------------------------------------------------------*/
